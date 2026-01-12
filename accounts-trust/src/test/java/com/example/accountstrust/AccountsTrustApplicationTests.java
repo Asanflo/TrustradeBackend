4b52b96f2e920e7ffa25cd2ac95130d4b6df8d48
Agassem Sanda Florentin <agassemflorentin@gmail.com>
2026-01-12 22:18:30 +0100

    chore:Initialisation of the repo

---
+++ accounts-trust/src/test/java/com/example/accountstrust/AccountsTrustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountstrust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsTrustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
