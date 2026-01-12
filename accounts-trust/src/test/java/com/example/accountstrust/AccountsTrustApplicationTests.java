9f1e963429ec30d91ed8a8fbe4d2ff565c31c482
Agassem Sanda Florentin <agassemflorentin@gmail.com>
2026-01-12 23:15:53 +0100

    chore:Configuration of the server

--- accounts-trust/src/test/java/com/example/accountstrust/AccountsTrustApplicationTests.java
+++ accounts-trust/src/test/java/com/example/accountstrust/AccountsTrustApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AccountsTrustApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
