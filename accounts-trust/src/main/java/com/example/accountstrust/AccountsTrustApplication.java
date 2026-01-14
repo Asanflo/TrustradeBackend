8c0424649f8dfd4cec8001e891f3b75dfb4eec0f
Agassem Sanda Florentin <agassemflorentin@gmail.com>
2026-01-14 12:30:46 +0100

    chore:setting gateway service

--- accounts-trust/src/main/java/com/example/accountstrust/AccountsTrustApplication.java
+++ accounts-trust/src/main/java/com/example/accountstrust/AccountsTrustApplication.java
@@ -2,12 +2,19 @@ package com.example.accountstrust;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AccountsTrustApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountsTrustApplication.class, args);
     }
 
+    @GetMapping("/accounts/")
+    public String sayHello() {
+        return "Bonjour ! Bienvenue sur AccountsTrust.";
+    }
 }
