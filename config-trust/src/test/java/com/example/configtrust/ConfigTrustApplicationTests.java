99fd434188e4cfc9bb8fddab7180ee654de4c79c
Agassem Sanda Florentin <agassemflorentin@gmail.com>
2026-01-14 23:20:48 +0100

    chore:setting config service

---
+++ config-trust/src/test/java/com/example/configtrust/ConfigTrustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configtrust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigTrustApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
